37fdfb781353fc08d662f9bdf8405bcd2f53c14c
Somesh Bhandare <someshbhandare3@gmail.com>
2023-07-16 00:42:16 +0530

    updated middleware

--- src/app/api/users/logout/route.ts
+++ src/app/api/users/logout/route.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(request: NextRequest){
+export async function GET(request: NextRequest, response: NextResponse){
     try {
         const response = NextResponse.json({
             message: "Logout success",
