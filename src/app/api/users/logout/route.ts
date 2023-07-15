1f5ded5524657452eb3691d528dbdbbe408edb76
Somesh Bhandare <someshbhandare3@gmail.com>
2023-07-16 02:10:05 +0530

    updated route logout

--- src/app/api/users/logout/route.ts
+++ src/app/api/users/logout/route.ts
@@ -3,12 +3,15 @@ import { NextRequest, NextResponse } from "next/server";
 export async function GET(request: NextRequest, response: NextResponse){
     try {
         const response = NextResponse.json({
+            headers: {
+                cookie: request.headers.get("cookie")
+            },
             message: "Logout success",
             success: true
         }, {status: 200})
 
         // const {val, options} = request.cookies.getWithOptions("token")
-        response.cookies.set(process.env.ACCESS_TOKEN!, "", {expires: new Date(Date.now())})
+        // response.cookies.set(process.env.ACCESS_TOKEN!, "", {expires: new Date(Date.now())})
         response.cookies.delete(process.env.ACCESS_TOKEN!)
         return response;
     } 
