117876757dffffd159acfca0bb330f0751968d3f
Somesh Bhandare <someshbhandare3@gmail.com>
2023-07-16 00:45:26 +0530

    updated logout route

--- src/app/api/users/logout/route.ts
+++ src/app/api/users/logout/route.ts
@@ -6,7 +6,7 @@ export async function GET(request: NextRequest, response: NextResponse){
             message: "Logout success",
             success: true
         }, {status: 200})
-        response.cookies.set("token", "", {expires: Date.now()})
+        response.cookies.set("token", "", {expires: new Date(Date.now())})
         // response.cookies.delete("token")
         return response;
     } 
